9adf5add2ffc38573f7beaf97b94e32ccdf9155e
Janteli <120306377+Janteli@users.noreply.github.com>
2024-08-15 10:10:47 +0000

    Data Types

---
+++ 01basics/01_variables.js
@@ -0,0 +1,13 @@
+const accId = 1234
+let accEmail = "fake@gmail.com"
+var accPass = "123"
+accCity = "Pokhara"
+
+// accId = 2 not allowed const variable
+
+// console.log([accId, accEmail, accPass, accCity]);
+
+/*Prefered not to use var because of issue in block scope and functional scope */
+
+let state;
+state="Koshi"
---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+//  alert(3+3) it possible possible only in browser
+
+// documentation MDN originala tc39.es ECMA SCRIPT
+
+// DATA-TYPES
+
+let book = "Yalambar"
+let age = 18
+let isLoggedIn = false
+// PRIMITVE DATA TYPES
+
+// number range 
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// OBJECT
+console.log(typeof "Ghane")
+
+console.log(typeof null) //-> result object undefined results undefined
+
+
