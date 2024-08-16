c3e088e17a5b45d7d0b8aad60a51f0361719cc51
Janteli <120306377+Janteli@users.noreply.github.com>
2024-08-16 07:44:16 +0000

    comparision

--- 01basics/03_conversionOperations.js
+++ 01basics/03_conversionOperations.js
@@ -18,3 +18,12 @@ let conve = Boolean(bool) // result true -> if string true if empty string false
 
 let numb = 33
 let striNumb = String(numb)
+
+// Comparision
+
+console.log("2"> 1) // true coz string 2 gets converted to number itself
+
+console.log(null > 0) //false
+console.log(null == 0) //false
+console.log(null >= 0) //true
+
