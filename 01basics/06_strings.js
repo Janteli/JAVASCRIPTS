92c7ae991c03e0359bcf4ec5a5f301a81fdbf51f
Janteli <120306377+Janteli@users.noreply.github.com>
2024-08-18 09:52:20 +0000

    String

---
+++ 01basics/06_strings.js
@@ -0,0 +1,21 @@
+const gameName = new String('GOLONG')
+// String is an object {0:"G", 1: "O"}
+
+// console.log(gameName.__proto__) accessing prototype
+// console.log(gameName.length);
+// console.log(gameName.toUppercase());
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+
+// const newString = gameName.substring(0,4) -> results new string from oth index to excluding last 
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString)
+// const newString = "  hello  "
+// console.log(newString.trim())
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+// url.replace('%20', "-")
+// url.includes('hitesh)
+
+// url.split('-') conversion of string to an array split('separator', limit)
