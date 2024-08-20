4268e1ebd46d30d49b847cc1ed1ac37e420c69cb
Janteli <120306377+Janteli@users.noreply.github.com>
2024-08-20 10:16:26 +0000

    array 2nd

--- 02basics/01_Arrays.js
+++ 02basics/01_Arrays.js
@@ -13,27 +13,43 @@
 // newArr.pop() // remove last elem
 // console.log(newArr)
 
-let Arr = [1,2,3,4,5,6,7]
+// let Arr = [1,2,3,4,5,6,7]
 
-// Arr.unshift(0) //add to first
-// Arr.shift() //Removes first elem
+// // Arr.unshift(0) //add to first
+// // Arr.shift() //Removes first elem
 
-// console.log(Arr.includes(2))
-// console.log(Arr.indexOf(3))
+// // console.log(Arr.includes(2))
+// // console.log(Arr.indexOf(3))
 
-// const newArray = Arr.join() // converted into string
+// // const newArray = Arr.join() // converted into string
 
-console.log("A", Arr)
+// console.log("A", Arr)
 
-const myn1 = Arr.slice(1,4) // return new array with including first index excluding last index given
-console.log(myn1)
-console.log("B", Arr)
+// const myn1 = Arr.slice(1,4) // return new array with including first index excluding last index given
+// console.log(myn1)
+// console.log("B", Arr)
 
 
-const myn2 = Arr.splice(1,4) //retun new array with including first  and last index mentioned
-console.log(myn2)
-console.log("C", Arr)
+// const myn2 = Arr.splice(1,4) //retun new array with including first  and last index mentioned
+// console.log(myn2)
+// console.log("C", Arr)
 
-console.log(Arr) //slice doesnt manupulate original array but spice does
+// console.log(Arr) //slice doesnt manupulate original array but spice does
+
+const marvelHeros = ["Thor", "Ironman", "Spiderman"]
+const dcHeros = ["Superman", "Flash", "Batman"]
+
+// marvelHeros.push(dcHeros)
+
+// console.log(marvelHeros) // creates dcHeros as a nested Array within a marvelHeros ; assumes dcHeros as a single data [ 'Thor', 'Ironman', 'Spiderman', [ 'Superman', 'Flash', 'Batman' ] ]
+
+// let concatArr = marvelHeros.concat(dcHeros) 
+// console.log(concatArr) // results [ 'Thor', 'Ironman', 'Spiderman', 'Superman', 'Flash', 'Batman' ]
+
+const allNewHeros = [...marvelHeros, ...dcHeros] // results [ 'Thor', 'Ironman', 'Spiderman', 'Superman', 'Flash', 'Batman' ]
+
+const another_array = [1 ,2 ,3 , [4, 5, 6], 7, [ 8, 9, [4 , 5]]]
+const realAnother_array = another_array.flat(2)
+console.log(realAnother_array)
 
 
